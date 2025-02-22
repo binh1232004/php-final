138e2496e7754685de591820dd8de7185933056f
Binh <binhwork245@gmail.com>
2025-02-22 12:53:49 +0700

    chore: change english text to vietnamese

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,9 @@
 <x-guest-layout>
+    <div class="text-xl  text-red-600">
+        {{ __('Chức năng vẫn đang trong tình trạng phát triển') }}
+    </div>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Chúng tôi sẽ gửi link đến email của bạn để đặt lại mật khẩu cho bạn!') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +21,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Xác nhận') }}
             </x-primary-button>
         </div>
     </form>
